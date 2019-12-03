2b35d807f84b3402a2bad4b1514fae9c88ab863d
Jess Daniel <jessdaniel474@gmail.com>
2019-12-03 16:52:11 -0500

    added POST method to posts route

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -5,6 +5,7 @@ const db = require("../data/db");
 
 const router = express.Router();
 
+// GET all posts
 router.get("/", (req, res) => {
     db.find()
         .then(posts => {
@@ -14,5 +15,55 @@ router.get("/", (req, res) => {
             res.status(500).json({ errorMessage: "Server error", err });
     })
 })
+
+// GET specific post by id
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(post => {
+            if (post.length > 0) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The post information could not be retrieved.", err });
+    })
+})
+
+// GET comments of a specific post by id
+router.get("/:id/comments", (req, res) => {
+    const { id } = req.params;
+    db.findCommentById(id)
+        .then(comment => {
+            if (comment.length > 0) {
+                res.status(200).json(comment);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The comments information could not be retrieved.", err });
+    })
+})
+
+// POST a new post
+router.post("/", (req, res) => {
+    const postData = req.body;
+    if (postData.title && postData.contents) {
+        db.insert(postData)
+            .then(post => {
+                const newPost = { ...postData, ...post };
+                res.status(201).json(newPost);
+            })
+            .catch(err => {
+                res.status(500).json({ error: "There was an error while saving the post to the database", err });
+        })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post" });
+    }
+})
+
 
 module.exports = router;
